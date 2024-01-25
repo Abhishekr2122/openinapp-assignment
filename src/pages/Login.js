f5b4c16e90f72f2ee80a94f899ad6db08175d316
Abhishekr2122 <abhishekrshinde21@gmail.com>
2024-01-25 16:34:11 +0530

    Adding elements to the Loginpage

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,7 +8,30 @@ export default function Login() {
         left: "159px",
       }}
     >
-      <div style={{ width: "719.99px", height: "1024px" }}></div>
+      <div style={{ width: "719.99px", height: "1024px" }}>
+        <p
+          style={{
+            width: "205px",
+            height: "88px",
+            top: "462.37px",
+            left: "196.33px",
+            fontWeight: "700",
+            fontSize: "72px",
+            lineHeight: "87.77px",
+          }}
+        >
+          Base
+        </p>
+        <div
+          style={{
+            width: "299px",
+            height: "48px",
+            top: "909px",
+            left: "144.5px",
+            justifyContent: "space-between",
+          }}
+        ></div>
+      </div>
       <div style={{ width: "719.99px", height: "1024px" }}>
         <p
           style={{
@@ -111,6 +134,77 @@ export default function Login() {
               }}
             />
           </form>
+          <a
+            href="https://openinapp.com/"
+            target="_blank"
+            rel="noreferrer"
+            style={{
+              textDecoration: "none",
+              color: "#346BD4",
+              width: "136.12px",
+              height: "20.86px",
+              top: "627.22px",
+              left: "847.21px",
+              fontWeight: "400",
+              fontSize: "16px",
+              lineHeight: "19.2px",
+            }}
+          >
+            Forgot password
+          </a>
+          <button
+            style={{
+              backgroundColor: "#605BFF",
+              width: "356.77px",
+              height: "43.91px",
+              top: "670.03px",
+              left: "846.11px",
+            }}
+          >
+            <p
+              style={{
+                width: "111.93px",
+                height: "23.84px",
+                top: "679.91px",
+                left: "969.7px",
+                color: "#FFFFFF",
+                fontWeight: "700",
+                fontSize: "16px",
+                lineHeight: "19.5px",
+                textAlign: "center",
+              }}
+            >
+              Sign in
+            </p>
+          </button>
+          <div
+            style={{
+              width: "286.51px",
+              height: "20.86px",
+              top: "768.83px",
+              left: "881.24px",
+            }}
+          >
+            <p
+              style={{
+                color: "#858585",
+                fontWeight: "400",
+                fontSize: "16px",
+                lineHeight: "19.2px",
+                textAlign: "center",
+              }}
+            >
+              Don't have an account?
+            </p>
+            <a
+              href="https://openinapp.com/"
+              target="_blank"
+              rel="noreferrer"
+              style={{ textDecoration: "none", color: "#346BD4" }}
+            >
+              Register here
+            </a>
+          </div>
         </div>
       </div>
     </div>
